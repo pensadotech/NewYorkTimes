9e923ca90ede74711c4f68b8a7fd7d0df6fa5631
mansouriamn <38742344+mansouriamn@users.noreply.github.com>
2018-08-30 21:26:22 -0700

    4th Change

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -1,4 +1,4 @@
-var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=";
+var url = "https://api.nytimes.com/svc/archive/v1/2016/1.json?api-key=";
 var apiKey = "14bf828c4c8f4bd3865904dbcc2facb3"
 fullURL = url + apiKey;
 
@@ -12,13 +12,14 @@ $('#submit').on('click', function () {
     searchTerm = $('term').val()
     queryURL = searchURL + searchTerm;
     $.get(queryURL)
-    .then(function (request) {
-    request.response.docs.forEach(doc => {
-            $('#results').append(`
+        .then(function (request) {
+            request.response.docs.forEach(doc => {
+                //$('#results').empty();
+                $('#results').append(`
             <h4>${doc.headline.main}</h4>
+            <h4><a href="${doc.web_url}" target="_blank">${doc.web_url}</a></h4>
             `)
-        });
-    })
+            });
+        })
 
 })
-
